8bc77d0c96aaaac53731d9d7a7f565b98f5a9120
Malibu <joellement@gmail.com>
2025-03-03 11:26:41 +0100

    added footer and header to generated subsites

---
+++ src/app/(subsites)/[id]/footer.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import MailtoLink from "@/components/mailto";
+
+export default function Footer() {
+  return (
+    <footer className="m-2 py-4 border-t border-black">
+      <ul className="font-black md:text-2xl flex-wrap flex justify-between gap-2">
+        <li className="self-start">
+          <Link href="/impressum">Impressum</Link>
+        </li>
+        <li className="self-end">
+          <MailtoLink word="Kontakt" />
+        </li>
+        <li className="self-start">
+          <Link href="/datenschutz">Datenschutz</Link>
+        </li>
+        <li className="self-end">© R21 GbR 2024</li>
+      </ul>
+    </footer>
+  );
+}
--- src/app/(subsites)/[id]/layout.tsx
+++ src/app/(subsites)/[id]/layout.tsx
@@ -1,7 +1,16 @@
+import Header from "@/app/(subsites)/header";
+import Footer from "@/app/(subsites)/footer";
+
 export default function SubsiteLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="subsite-layout">{children}</div>;
+  return (
+    <div className="subsite-layout md:mb-8 mx-2 h-full md:w-full">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 }
